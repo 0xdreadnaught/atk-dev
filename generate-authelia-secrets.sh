92b290a6af6c2a0933b1a3a7cd7be6b2cb5cdcb5
0xdreadnaught <brianrpeters@gmail.com>
2023-08-30 23:28:30 -0400

    reset

--- generate-authelia-secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Generate JWT_SECRET
-echo $(openssl rand -base64 32) > ./atk-auth/secrets/JWT_SECRET
-
-# Generate SESSION_SECRET
-echo $(openssl rand -base64 32) > ./atk-auth/secrets/SESSION_SECRET
-
